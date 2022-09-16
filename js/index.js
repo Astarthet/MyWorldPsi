2790d9756c5fa85e9663190ef6e6b8e42ebf2717
Astarthet <kevinhernandezhb@hotmail.com>
2022-09-16 13:38:26 -0500

    snapshot_1

---
+++ js/index.js
@@ -0,0 +1,28 @@
+window.onscroll = () => {
+  scroll = document.documentElement.scrollTop;
+  header = document.getElementById("header");
+  if (scroll > 20) {
+    header.classList.add("nav-move");
+  } else if (scroll < 20) {
+    header.classList.remove("nav-move");
+  }
+};
+
+document.getElementById("btn-menu").addEventListener("click", mostrar_Menu);
+menu = document.getElementById("header");
+body = document.getElementById("container-all");
+nav = document.getElementById("nav");
+function mostrar_Menu() {
+  menu.classList.toggle("move-content");
+  body.classList.toggle("move-content")
+  nav.classList.toggle("move-nav");
+}
+
+
+window.addEventListener("resize", function(){
+  if(window.innerWidth > 760 ){
+    menu.classList.remove("move-content");
+    body.classList.remove("move-content")
+    nav.classList.remove("move-nav");
+  }
+})
