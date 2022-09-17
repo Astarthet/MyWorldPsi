f1ff796b9333759a4fee4cfee1bfe94d5e7f5b79
Astarthet <kevinhernandezhb@hotmail.com>
2022-09-17 10:35:53 -0500

    snapshot_3

--- js/index.js
+++ js/index.js
@@ -14,15 +14,22 @@ body = document.getElementById("container-all");
 nav = document.getElementById("nav");
 function mostrar_Menu() {
   menu.classList.toggle("move-content");
-  body.classList.toggle("move-content")
+  body.classList.toggle("move-content");
   nav.classList.toggle("move-nav");
 }
 
-
-window.addEventListener("resize", function(){
-  if(window.innerWidth > 760 ){
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 760) {
     menu.classList.remove("move-content");
-    body.classList.remove("move-content")
+    body.classList.remove("move-content");
     nav.classList.remove("move-nav");
   }
-})
+});
+
+/* let lg = document.getElementById("logoCss");
+let bg = document.getElementById("body");
+function bodyCss() {
+  if (lg.click) {
+    bg.style.background = "#8da0a4";
+  }
+} */
